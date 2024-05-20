f3e5fce51427395f92d22295de7d77b2ea6286b4
pjh753156a <00-g-00@naver.com>
2024-05-20 10:48:17 +0900

    2024-05-20-79days

--- src/App.tsx
+++ src/App.tsx
@@ -30,21 +30,21 @@ function App() {
   //   render   //
   return (
     <Routes>
-      {/* <Route index element={<Index />} />
-      <Route path={SNS_PATH} element={<Sns />} />
+      <Route index element={<Index />} />
+      {/* <Route path={SNS_PATH} element={<Sns />} />
       <Route path={AUTH_PATH} element={<Authentication />} />
         <Route path={SIGN_IN_PATH} element={<SignIn />} />
         <Route path={SIGN_UP_PATH} element={<SignUp />} />
         <Route path={BUSINESS_REGISTRATION_PATH} element={<BusinessRegistration />} />
         <Route path={FIND_EMAIL_PATH} element={<FindEmail />} />
         <Route path={FIND_PASSWORD_PATH} element={<FindPassword />} />
-        <Route path={PASSWORD_RESET_PATH} element={<PasswordReset />} />
+        <Route path={PASSWORD_RESET_PATH} element={<PasswordReset />} /> */}
       <Route path={RESTAURANT_PATH} element={<Restaurant />} />
         <Route path={RESTAURANT_LIST_PATH} element={<RestaurantList />} />
         <Route path={RESTAURANT_INFO_PATH} element={<RestaurantInfo />} />
         <Route path={RESTAURANT_RESERVATION_PATH} element={<RestaurantReservation />} />
         <Route path={RESTAURANT_RESERVATION_INFO_PATH} element={<RestaurantReservationInfo />} />
-      <Route path={MYPAGE_PATH} element={<MyPage />} />
+      {/* <Route path={MYPAGE_PATH} element={<MyPage />} />
         <Route path={USER_INFO_UPDATE_PATH} element={<UserInfoUpdate />} />
         <Route path={USER_DELETE_PATH} element={<UserDelete />} />
       <Route path={BOARD_PATH} element={<Board />} />
@@ -56,7 +56,7 @@ function App() {
         <Route path={NOTICE_PATH} element={<Notice />} />
           <Route path={NOTICE_BOARD_WRITE_PATH} element={<NoticeWrite />} />
           <Route path={NOTICE_BOARD_UPDATE_PATH} element={<NoticeUpdate />} />
-          <Route path={NOTICE_DETAILS_PATH} element={<NoticeDetails />} /> */}
+          <Route path={NOTICE_DETAILS_PATH} element={<NoticeDetails />} />  */}
     </Routes>
   );
 }
