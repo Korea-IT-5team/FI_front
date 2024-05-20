46b847e68521015173061df360be536d568e64c9
developernagk <nagk907@gmail.com>
2024-05-20 10:15:19 +0900

    240520 apis request dto UPDATE

--- src/apis/restaurant/reservation/dto/request/index.ts
+++ src/apis/restaurant/reservation/dto/request/index.ts
@@ -5,6 +5,7 @@ export interface PostReservationRequestDto {
     reservationPeople: number;
 }
 
+// description: 식당 예약 취소 Request Body DTO
 export interface DeleteReservationRequestDto {
     cancellationReason: string;
 }
