ec5d90b28becfa074081b15a4fc1c87f320a3ded
roqkfwk87 <tuicswrjj86874@naver.com>
2024-05-27 15:49:15 +0900

    20240527

--- src/views/Authentication/BusinessRegistration/index.tsx
+++ src/views/Authentication/BusinessRegistration/index.tsx
@@ -1,25 +1,89 @@
 import { useNavigate } from "react-router";
-import { SIGN_IN_ABSOLUTE_PATH } from "src/constant";
+import { BUSINESS_REGISTRATION_ABSOLUTE_PATH, SIGN_IN_ABSOLUTE_PATH } from "src/constant";
 import { useAuthStore } from "src/stores";
 import "./style.css";
 import InputBox from "src/components/InputBox";
 import { useState } from "react";
+import { SignUpRequestDto } from "src/apis/auth/dto/request";
+import { signUpRequest } from "src/apis/auth";
+import ResponseDto from "src/apis/response.dto";
 
 //   component: 사업자등록번호 확인   //
 export default function BusinessRegistration() {
 
     // state //
-    const [BusinessRegistration, setBusinessRegistration] = useState<string>('');
-    const [BusinessRegistrationStatus, setBusinessRegistrationStatus] = useState<boolean>(false);
-    const [BusinessRegistrationCheck, setBusinessRegistrationCheck] = useState<boolean>(false);
-    const [BusinessRegistrationError, setBusinessRegistrationError] = useState<boolean>(false);
+    const { emailId, password, nickname, userTelNumber, authNumber, userAddress, userName, joinPath, snsId } = useAuthStore();
+
+    const [businessRegistration, setBusinessRegistration] = useState<string>('');
+    const [businessRegistrationStatus, setBusinessRegistrationStatus] = useState<boolean>(false);
+    const [businessRegistrationCheck, setBusinessRegistrationCheck] = useState<boolean>(false);
+    const [businessRegistrationError, setBusinessRegistrationError] = useState<boolean>(false);
 
     // function //
     const navigator = useNavigate();
 
+    const signUpResponse = (result: ResponseDto | null) => {
+
+        const message = 
+            !result ? '서버에 문제가 있습니다.' :
+            result.code === 'VF' ? '입력 형식이 맞지 않습니다.' : 
+            result.code === 'DE' ? '중복된 이메일입니다.' :
+            result.code === 'DN' ? '중복된 닉네임입니다.' :
+            result.code === 'SF' ? '인증번호 전송 실패했습니다.' :
+            result.code === 'AF' ? '인증번호가 일치하지 않습니다.' :
+            result.code === 'DBE' ? '서버에 문제가 있습니다.' : '' ;
+    
+        const isSuccess = result && result.code === 'SU';
+        if (!isSuccess) {
+            alert(message);
+            return;
+        }
+    
+        navigator(BUSINESS_REGISTRATION_ABSOLUTE_PATH);
+    
+    };
+
+    // TODO: 사업자 등록번호 인증 요청 응답 처리 함수
+    const _ = () => {
+        // TODO: 응답 처리 
+        // TODO: 실패 처리
+
+        // TODO: 성공일 때 
+        const requestBody: SignUpRequestDto = {
+            userEmailId: emailId,
+            password: password,
+            nickname: nickname,
+            userName: userName,
+            userTelNumber: userTelNumber,
+            authNumber: authNumber,
+            userAddress: userAddress,
+            joinPath,
+            snsId,
+            businessRegistration
+        }
+        signUpRequest(requestBody).then(signUpResponse);
+    };
+
     // event handler //
     const onBusinessRegistrationButtonClickHandler = () => {
-        if (!BusinessRegistrationStatus) return;
+        if (!businessRegistrationStatus) return;
+        // TODO: 사업자 등록번호 인증 요청
+
+    }
+
+    const onUserLinkClickHandler = () => {
+        const requestBody: SignUpRequestDto = {
+            userEmailId: emailId,
+            password: password,
+            nickname: nickname,
+            userName: userName,
+            userTelNumber: userTelNumber,
+            authNumber: authNumber,
+            userAddress: userAddress,
+            joinPath,
+            snsId
+        }
+        signUpRequest(requestBody).then(signUpResponse);
     }
 
     //   render   //
@@ -30,10 +94,10 @@ export default function BusinessRegistration() {
                 <div className='BusinessRegistration-box'>
                     <div className='BusinessRegistration-confirm-box'>
 
-                        <InputBox type="text" value={BusinessRegistration} placeholder="사업자등록번호" buttonTitle="인증" buttonStatus={BusinessRegistrationStatus} onButtonClickHandler={onBusinessRegistrationButtonClickHandler} />
+                        <InputBox type="text" value={businessRegistration} placeholder="사업자등록번호" buttonTitle="인증" buttonStatus={businessRegistrationStatus} onButtonClickHandler={onBusinessRegistrationButtonClickHandler} />
 
                     </div>
-                    <div className='text-link-title' onClick={() => { navigator(SIGN_IN_ABSOLUTE_PATH) }}>일반회원일 경우 넘어가기</div>
+                    <div className='text-link-title' onClick={onUserLinkClickHandler}>일반회원일 경우 넘어가기</div>
                 </div>
             </div>
         </div>
