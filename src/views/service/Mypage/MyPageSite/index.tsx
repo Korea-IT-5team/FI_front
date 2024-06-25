f789bd732a69ad261b0d923e8fe9aa75de978e1a
pjh <00-g-00@naver.com>
2024-06-26 01:57:37 +0900

    240626srcupdate

--- src/views/service/Mypage/MyPageSite/index.tsx
+++ src/views/service/Mypage/MyPageSite/index.tsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router';
+
+import { useEffect, useState } from 'react';
+
 import ResponseDto from 'src/apis/response.dto';
-import { getMyInfoRequest } from 'src/apis/user';
 import { GetMyInfoResponseDto } from 'src/apis/user/dto/response';
-import { INQUIRY_MY_BOARD_LIST_ABSOLUTE_PATH, MAIN_ABSOLUTE_PATH, MY_PAGE_SITE_ABSOLUTE_PATH, RESTAURANT_FAVORITE_ABSOLUTE_LIST_PATH, RESTAURANT_RESERVATION_ABSOLUTE_LIST_PATH, RESTAURANT_REVIEW_ABSOLUTE_DETAILS_LIST_PATH, USER_DELETE_ABSOLUTE_PATH, USER_INFO_UPDATE_ABSOLUTE_PATH} from 'src/constant';
+
+import { getMyInfoRequest } from 'src/apis/user';
+
+import { INQUIRY_MY_BOARD_LIST_ABSOLUTE_PATH, MAIN_ABSOLUTE_PATH, MY_PAGE_SITE_ABSOLUTE_PATH, RESTAURANT_FAVORITE_ABSOLUTE_LIST_PATH, RESTAURANT_RESERVATION_ABSOLUTE_LIST_PATH, RESTAURANT_REVIEW_ABSOLUTE_DETAILS_LIST_PATH, USER_DELETE_ABSOLUTE_PATH, USER_INFO_UPDATE_ABSOLUTE_PATH } from 'src/constant';
+
 import "./style.css";
 
 // component: 마이페이지 //
@@ -12,23 +17,16 @@ export default function MyPageSite() {
 
   // state //
   const [cookies] = useCookies();
-
-  const [userEmailId, setEmailId] = useState<string>('');
+  const [userRole, setUserRole] = useState<string>('');
   const [nickname, setNickname] = useState<string>('');
   const [userName, setUserName] = useState<string>('');
+  const [userEmailId, setEmailId] = useState<string>('');
   const [userTelNumber, setUserTelNumber] = useState<string>('');
-  const [userAddress, setUserAddress] = useState<string>('');
-  const [userRole, setUserRole] = useState<string>('');
-
+  
   // function //
   const navigation = useNavigate();
 
   const GetMyInfoResponse = (result : GetMyInfoResponseDto | ResponseDto | null) => {
-    const message =
-      !result ? '서버에 문제가 있습니다.' :
-      result.code === 'AF' ? '인증에 실패했습니다.' :
-      result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
-
     if (!result || result.code !== 'SU') {
       if (result?.code === 'AF') {
         navigation(MAIN_ABSOLUTE_PATH);
@@ -40,14 +38,22 @@ export default function MyPageSite() {
 
     if (!cookies.accessToken) return;
 
-    const {userEmailId, nickname, userName, userTelNumber, userAddress} = result as GetMyInfoResponseDto;
+    const {userEmailId, nickname, userName, userTelNumber} = result as GetMyInfoResponseDto;
     setNickname(nickname);
     setEmailId(userEmailId);
     setUserName(userName);
     setUserTelNumber(userTelNumber);
-    setUserAddress(userAddress);
     setUserRole(userRole);
   };
+
+  // event handler //
+  const onMyPageSiteClickHandler = () => navigation(MY_PAGE_SITE_ABSOLUTE_PATH);
+  const onUserInfoUpdateClickHandler = (userEmailId:string) => navigation(USER_INFO_UPDATE_ABSOLUTE_PATH(userEmailId));
+  const onUserDeleteClickHandler = (userEmailId:string) => navigation(USER_DELETE_ABSOLUTE_PATH(userEmailId));
+  const onRestaurantFavoriteClickHandler = () => navigation(RESTAURANT_FAVORITE_ABSOLUTE_LIST_PATH);
+  const onRestaurantReservationListClickHandler = () => navigation(RESTAURANT_RESERVATION_ABSOLUTE_LIST_PATH);
+  const onRestaurantReviewDetailsClickHandler = () => navigation(RESTAURANT_REVIEW_ABSOLUTE_DETAILS_LIST_PATH);
+  const onInquiryMyBoardListClickHandler = () => navigation(INQUIRY_MY_BOARD_LIST_ABSOLUTE_PATH);
 
   //   effect   //
   useEffect(() => {
@@ -63,16 +69,16 @@ export default function MyPageSite() {
           <div className='short-divider-line'></div>
         </div>
         <div className='my-page-navigation-box'>
-          <div className='my-page-navigation' onClick={() => navigation(MY_PAGE_SITE_ABSOLUTE_PATH)}>마이페이지</div>
-          <div className='my-page-navigation' onClick={() => navigation(USER_INFO_UPDATE_ABSOLUTE_PATH(userEmailId))}>회원정보 수정</div>
-          <div className='my-page-navigation' onClick={() => navigation(USER_DELETE_ABSOLUTE_PATH(userEmailId))}>회원탈퇴</div>
+          <div className='my-page-navigation' onClick={onMyPageSiteClickHandler}>마이페이지</div>
+          <div className='my-page-navigation' onClick={() => onUserInfoUpdateClickHandler(userEmailId)}>회원정보 수정</div>
+          <div className='my-page-navigation' onClick={() => onUserDeleteClickHandler(userEmailId)}>회원탈퇴</div>
         </div>
         <div className='short-divider-line'></div>
         <div className='my-page-contents-nav-box'>
-          <div className='my-page-contents-nav' onClick={() => navigation(RESTAURANT_FAVORITE_ABSOLUTE_LIST_PATH)}>찜한 식당 목록</div>
-          <div className='my-page-contents-nav' onClick={() => navigation(RESTAURANT_RESERVATION_ABSOLUTE_LIST_PATH)}>예약 내역</div>
-          <div className='my-page-contents-nav' onClick={() => navigation(RESTAURANT_REVIEW_ABSOLUTE_DETAILS_LIST_PATH)}>리뷰 내역</div>
-          <div className='my-page-contents-nav' onClick={() => navigation(INQUIRY_MY_BOARD_LIST_ABSOLUTE_PATH)}>내 문의내역</div>
+          <div className='my-page-contents-nav' onClick={onRestaurantFavoriteClickHandler}>찜한 식당 목록</div>
+          <div className='my-page-contents-nav' onClick={onRestaurantReservationListClickHandler}>예약 내역</div>
+          <div className='my-page-contents-nav' onClick={onRestaurantReviewDetailsClickHandler}>리뷰 내역</div>
+          <div className='my-page-contents-nav' onClick={onInquiryMyBoardListClickHandler}>내 문의내역</div>
         </div>
         <div className='short-divider-bottom-line'></div>
         <div className='my-page-info-contents-container'>
